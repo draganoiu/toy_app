07f8671fb1b4bdcc93402de904294f994fc5820e
toy_app <andrei_draganoiu@yahoo.com>
2016-10-09 00:23:39 +0300

    Add user from interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+ # resources :users, except: [:new]
 
 
 root  'pages#homepage'
 get   'about',    to: 'pages#about'
 
 resources :articoles
+
+get 'signup', to: 'users#new'
+post 'users', to: 'users#create'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
